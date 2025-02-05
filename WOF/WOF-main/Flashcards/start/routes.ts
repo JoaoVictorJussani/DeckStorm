34bab837e75a4774d9fce07864f70825d535062b
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-02-05 15:47:28 +0100

    feat(Flashcards)

--- WOF/WOF-main/Flashcards/start/routes.ts
+++ WOF/WOF-main/Flashcards/start/routes.ts
@@ -10,3 +10,7 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/login', async ({ view }) => {
+    return view.render('pages/login')
+  })
