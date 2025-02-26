69afc8d8c32649ac79cfe084c4fee0b22db3b346
João Victor <pg05lby@eduvaud.ch>
2025-02-26 14:39:06 +0100

    feat: commit de secours

---
+++ config.js
@@ -0,0 +1,6 @@
+
+          var iceScrumToken = "e981311724ae43c099dee969cf9b4a09";
+          projectId = "WOFJDA2024";
+          masterProjectId = "WOFMAINGCR";
+          const API_base_url = "https://etml.icescrum.com/ws/project/";
+          
